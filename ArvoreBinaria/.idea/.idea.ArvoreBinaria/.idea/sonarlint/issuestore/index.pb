a4a042d1021b2b8d575f1aaaebc08291c91d7eed
jose.santos <jose.junior@napista.com.br>
2023-09-29 13:48:37 -0300

    Arvore binaria

---
+++ ArvoreBinaria/.idea/.idea.ArvoreBinaria/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+H
+ArvoreBinaria/Program.cs,e\b\eb4e0fef6d488c86268fc70581cd3e7d45a1fa16
---
+++ ArvoreBinaria/ArvoreBinaria/Program.cs
@@ -0,0 +1,65 @@
+﻿var arvore = Node.Initialize();
+arvore = Node.Insert(arvore, 1);
+arvore = Node.Insert(arvore, 5);
+arvore = Node.Insert(arvore, 2);
+arvore = Node.Insert(arvore, 17);
+arvore = Node.Insert(arvore, 4);
+
+Node.TreePrint(arvore);
+Console.WriteLine();
+
+class Node
+{
+    public int Elemento { get; set; }
+    public Node? Left { get; set; }
+    public Node? Right { get; set; }
+
+    public static Node? Initialize()
+    {
+        return null;
+    }
+
+    public Node(int elemento)
+    {
+        Elemento = elemento;
+        Left = null;
+        Right = null;
+    }
+
+    public static void TreePrint(Node? no)
+    {
+        if (no != null)
+        {
+            Console.WriteLine(no.Elemento);
+            TreePrint(no.Right);
+            TreePrint(no.Left);
+        }
+    }
+    
+    public static void TreeFree(Node? no)
+    {
+        if (no != null)
+        {
+            TreePrint(no.Right);
+            TreePrint(no.Left);
+            TreeFree(no);
+        }
+    }
+    
+    public static Node Insert(Node? no, int elemento)
+    {
+        if (no == null) // Se for nulo é a raiz
+        {
+            var node =  new Node(elemento);
+            return node;
+        }
+
+        if (elemento >= no.Elemento)
+            no.Right = Insert(no.Right, elemento);
+
+        else if (elemento < no.Elemento)
+            no.Left = Insert(no.Left, elemento);
+
+        return no;
+    }
+}
